8b7d9601efbeb7deaa45d2ca47a2aae9ecca87d4
Sanjit Grover <sanjit.grover@gmail.com>
2022-11-29 02:53:31 +0000

    Intial Dynamo Table

---
+++ cdk.out/asset.e8a6c82a1b7612ee0e7f066551b44e8498b79ac1a3397598d9f83690bc0f5614/hello.js
@@ -0,0 +1,9 @@
+//Test Function
+
+
+export async function main(event, context){
+    return {
+            statusCode: 200,
+            body: 'Hello from Prem!'
+    }
+}
--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,8 +2,14 @@ import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+// import {RestApi, LambdaIntegration} from 'aws-cdk-lib/aws-apigateway';
+import {GenericTable} from './GenericTable';
 
 export class SpaceStack extends Stack{
+    
+    // private api = new RestApi(this, 'SpaceApi');
+    private spacesTable=new GenericTable('SpaceTable', 'spaceId', this);
+    
     constructor(scope: Construct, id: string, props: StackProps){
         super(scope, id, props)
 
